04cbee24e756b73ad1b9a16a1c3d3792ce240e4d
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-16 20:14:00 +0500

    updated

--- .github/actions/deploy-to-server/main.js
+++ .github/actions/deploy-to-server/main.js
@@ -3,7 +3,15 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-  core.notice("My first js actions")
+	const bucket = core.getInput('bucket', { required: true });
+	const bucketRegion = core.getInput('bucket-region', { required: true });
+	const distDoler = core.getInput('dist-folder', { required: true });
+
+	const s3Uri = `s3://${bucket}`;
+	exec.exec(`aws s3 sync ${distDoler} ${s3Uri} --region ${bucketRegion}`);
+
+	const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazon.com`;
+  core.setOutput('website-url', websiteURL)
 }
 
 run();
